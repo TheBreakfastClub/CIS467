0d96abe078db4578183a9838d33c92813555866c
Tyler Hutek <hutekt@mail.gvsu.edu>
2014-10-13 13:09:25 -0400

    Create character.h

---
+++ code/engine/world_definition/character.h
@@ -0,0 +1,23 @@
+/*********************************************************
+File Name: character.h
+Author: The Breakfast Club
+Creation Date: 10-13-2014
+Description: Defines the different types of characters that can
+be in the world.
+************************************************************/
+#pragma once
+#include <utility>
+
+using namespace std;
+
+class Character {
+  public:
+    /** Defines the amount of hitpoints a sprite has **/
+    int hitPoints;
+    /** Defines if a sprite is invincible **/
+    bool invincible;
+    /** Defines the speed of a sprite **/
+    int speed;
+    /** Defines the attack damage of a sprite **/
+    int attackDmg;
+};
