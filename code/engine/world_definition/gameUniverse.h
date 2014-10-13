75397da2a161bdaf8e9da42ca866e09ec7435643
Tyler Hutek <hutekt@mail.gvsu.edu>
2014-10-13 12:47:06 -0400

    Create gameUniverse.h

---
+++ code/engine/world_definition/gameUniverse.h
@@ -0,0 +1,14 @@
+// A collection of all sublevls including the hub.
+
+
+#pragma once
+
+#include "gameWorld.h"
+#include "hero.h"
+#include "sublevel.h"
+
+class GameUniverse{
+  public:
+    GameWorld[Sublevel::COUNT] sublevels;
+    
+};
