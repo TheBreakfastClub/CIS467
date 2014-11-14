16e41a9966a5968f4b1aae8ad35a64e7fc524521
tash1 <tashi@neuron0.net>
2014-11-14 17:52:06 -0500

    Create image.h

---
+++ code/engine/render/image.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include "types.h"
+
+class Image {
+	private:
+	bool dealloc;
+	void init(int, int, void*, bool);
+	
+	public:
+	int w,h;
+	u32 *pixels;
+	float cx, cy;
+	
+	Image();
+	Image(int, int);
+	Image(int, int, void*);
+	~Image();
+	
+	void resize(int, int);
+	void clear();
+	void clear(u32);
+  static u32 blend(u32, u32);
+  bool collision(Image*, int, int);
+  bool rcollision(Image*, int, int, float);
+  bool scollision(Image*, int, int, float);
+  bool rscollision(Image*, int, int, float, float);
+  int overlap(Image*, int, int);
+  int roverlap(Image*, int, int, float);
+  int soverlap(Image*, int, int, float);
+  int rsoverlap(Image*, int, int, float, float);
+	void _blit(Image*, int, int);
+	void _ablit(Image*, int, int);	
+  void blit(Image*, int, int);
+	void ablit(Image*, int, int);
+	void scaleblit(Image*);
+	void ascaleblit(Image*);	
+	void asblit(Image*, int, int, float);
+	void arblit(Image*, float, float, float);
+	void arsblit(Image*, float, float, float, float);
+	void putchar(unsigned char, int, int, u32);
+	void puttext(const char*, int, int, u32);
+	void puttext(std::string, int, int, u32);
+};
