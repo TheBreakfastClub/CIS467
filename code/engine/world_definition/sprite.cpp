a607c32f71dea325f238ca4ca23cee3a1b80baac
tash1 <tashi@neuron0.net>
2014-12-05 13:45:27 -0500

    Update sprite.cpp

--- code/engine/world_definition/sprite.cpp
+++ code/engine/world_definition/sprite.cpp
@@ -27,9 +27,9 @@ Sprite::Sprite(int x, int y, Image *spriteImageH, Image *spriteImageM, Image *sp
  */
 Sprite::~Sprite() {
     if (deallocNeeded && spriteImageH) {
+        if(spriteImageM != spriteImageH) delete spriteImageM;
+        if(spriteImageL != spriteImageH) delete spriteImageL;
         delete spriteImageH;
-        delete spriteImageM;
-        delete spriteImageL;
     }
 }
 
@@ -54,7 +54,7 @@ Image* Sprite::getSpriteImage(Resolution res) {
  */
 bool Sprite::setSpriteImage(const char *imgFileName, pixAlgo pixelator, int medCut, int lowCut) {
 
-    if (imgFileName == NULL || pixelator == NULL) return false;
+    if (imgFileName == NULL) return false;
 
     // Deallocate old images if needed
     if (spriteImageH && deallocNeeded) {
@@ -64,22 +64,23 @@ bool Sprite::setSpriteImage(const char *imgFileName, pixAlgo pixelator, int medC
     }
 
     deallocNeeded = true;
-
+    
     // Read in the high resolution image
     spriteImageH = loadImage(imgFileName);
     if (!spriteImageH) return false;
-
-    // Create Medium resolution image
-    Image *shrunken = downsample(spriteImageH, spriteImageH->w/medCut, spriteImageH->h/medCut, pixelator);
-    spriteImageM = new Image(spriteImageH->w, spriteImageH->h);
-    spriteImageM->scaleblit(shrunken);
-    delete shrunken;
     
-    // Create Low resolution image
-    shrunken = downsample(spriteImageH, spriteImageH->w/lowCut, spriteImageH->h/lowCut, pixelator);
-    spriteImageL = new Image(spriteImageH->w, spriteImageH->h);
-    spriteImageL->scaleblit(shrunken);
-    delete shrunken;
+    scale[0] = lowCut;
+    scale[1] = medCut;
+    scale[2] = 1;
+
+    if(pixelator) {
+      // Create Medium resolution image
+      spriteImageM = downsample(spriteImageH, spriteImageH->w/medCut, spriteImageH->h/medCut, pixelator);
+    
+      // Create Low resolution image
+      spriteImageL = downsample(spriteImageH, spriteImageH->w/lowCut, spriteImageH->h/lowCut, pixelator);
+    }
+    else spriteImageL = spriteImageM = spriteImageH;
 
     return true;
 }
@@ -94,9 +95,9 @@ void Sprite::setSpriteImage(Image *spriteImageHigh, Image *spriteImageMed, Image
 
     // Deallocate old images if needed
     if (spriteImageH && deallocNeeded) {
+        if(spriteImageM != spriteImageH) delete spriteImageM;
+        if(spriteImageL != spriteImageH) delete spriteImageL;
         delete spriteImageH;
-        delete spriteImageM;
-        delete spriteImageL;
     }
 
     deallocNeeded = false;
